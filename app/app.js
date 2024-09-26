f730a82168cfdf6138ee733d9183f8421577bdad
anchalBomjan <anchal.percoidit@gmail.com>
2024-09-26 16:04:33 +0545

    Adding controller and click directive and other

--- app/app.js
+++ app/app.js
@@ -3,31 +3,57 @@ var  myNinjaApp=angular.module('myNinjaApp',[]);
 //To protect the variable from manification we do like this  ways
 
 myNinjaApp.controller('NinjaController',['$scope',function($scope){
-$scope.message="hey y all";
-$scope.fav="test"
 
-// $scope.ninjas=['yoshi','crystal','ryu','shaun'];
+$scope.removeNinja=function(ninja)
+{
+    var removedNinja=$scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja,1);
+};
+$scope.addNinja=function(){
+
+    $scope.ninjas.push({
+        name:$scope.newninja.name,
+        belt:$scope.newninja.belt,
+        rate:parseInt($scope.newninja.rate),
+        available:true
+
+    });
+
+
+    $scope.newninja.name="";
+    $scope.newninja.belt="";
+    $scope.newninja.rate="";
+};
+
 $scope.ninjas=[
     {
         name:'Yoshi',
         belt:'Green',
-        rate:50
+        rate:50,
+        available:true,
+        thumb:"../content/img/Yoshi.jpg"
     },
     {
         name:'Crystal',
         belt:'yellow',
-        rate:30
+        rate:30,
+        available:true,
+         thumb:"../content/img/Crystal.jpg"
     },
     {
         name:'Ryu',
-        belt:'Orange',
-        rate:10
+        belt:'Green',
+        rate:10,
+        available:true,
+        thumb:"../content/img/Ryu.jpg"
 
     },
     {
         name:'Shaun',
         belt:'black',
-        rate:1000
+        rate:1000,
+        available:true,
+        thumb:"../content/img/shaun.jpg"
     }
 ]
 }]);
